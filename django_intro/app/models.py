fd039cbc82be72e97c1e4c139bb9a600f5431107
Chand <2017eeb1136@iitrpr.ac.in>
2020-04-02 23:37:04 +0530

    superuser creatd

--- django_intro/app/models.py
+++ django_intro/app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+import datetime
 
 
 class Question(models.Model):
@@ -16,4 +17,4 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
     def __str__(self):
-       return self.choice_tex
+       return self.choice_text
