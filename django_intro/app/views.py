789a4d8744129ff9c8f46885c177f3086dcac60c
Chand <2017eeb1136@iitrpr.ac.in>
2020-04-03 17:17:41 +0530

    Done (with generic views)

--- django_intro/app/views.py
+++ django_intro/app/views.py
@@ -5,34 +5,32 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {
-        'latest_question_list': latest_question_list,
-        'message' : "(Home Page) Hello, world. You're at the App's index."
-    }
-    # template = loader.get_template('app/index.html')
-    # return HttpResponse(template.render(context, request))
-    return render(request, 'app/index.html', context)
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {
+#         'latest_question_list': latest_question_list,
+#         'message' : "(Home Page) Hello, world. You're at the App's index."
+#     }
+#     # template = loader.get_template('app/index.html')
+#     # return HttpResponse(template.render(context, request))
+#     return render(request, 'app/index.html', context)
 
 
-def detail(request, question_id):
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    # return render(request, 'app/detail.html', {'question': question})
+# def detail(request, question_id):
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question does not exist")
+#     # return render(request, 'app/detail.html', {'question': question})
 
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'app/detail.html', {'question': question})
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'app/detail.html', {'question': question})
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'app/results.html', {'question': question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'app/results.html', {'question': question})
 
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
