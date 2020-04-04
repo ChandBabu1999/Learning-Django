c6218b8728b136ae17a2dfa746126326945fd8c7
Chand <2017eeb1136@iitrpr.ac.in>
2020-04-04 14:22:13 +0530

    adding css

--- django_intro/app/views.py
+++ django_intro/app/views.py
@@ -60,7 +60,6 @@ class IndexView(generic.ListView):
         """Return the last five published questions."""
         return Question.objects.order_by('-pub_date')[:5]
 
-
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'app/detail.html'
